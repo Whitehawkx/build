bbe5ad4a8929bec57ed57d0e01c4c4871a74f45e
lithid <long.jeremie@gmail.com>
2012-01-29 17:07:19 -0500

    Allow low memory machine to build with a smaller java emulator.
    This allows the default for 64 bit machines and adjusts the size
    on lower memory machines. If you are on 32 bit you can't build
    without this change, even using pae kernel with high mem.
    
    Patch 5: found a better way to do this. Removed import string.
    Change-Id: Iac1ef3b5db96029ca62897f6b64646bf6224e752

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -461,8 +461,13 @@ def SignFile(input_name, output_name, key, password, align=None,
   else:
     sign_name = output_name
 
-  cmd = [OPTIONS.java_path, "-Xmx2048m", "-jar",
+  check = (sys.maxsize > 2**32)
+  if check is True:
+    cmd = ["java", "-Xmx2048m", "-jar",
          os.path.join(OPTIONS.search_path, OPTIONS.signapk_path)]
+  else:
+    cmd = ["java", "-Xmx1024m", "-jar",
+           os.path.join(OPTIONS.search_path, "framework", "signapk.jar")]
   cmd.extend(OPTIONS.extra_signapk_args)
   if whole_file:
     cmd.append("-w")
